c9f082721b668ad7942793f8df89646acc22c62d
Shen Kai <shenkai2@lenovo.com>
2017-02-14 11:17:15 +0800

    domain

--- src/app/domain/domain.component.ts
+++ src/app/domain/domain.component.ts
@@ -1,14 +1,35 @@
 /**
  * Created by duanxc1 on 1/13/2017.
  */
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 
 @Component({
   selector: 'domain',
   templateUrl: './domain.component.html',
   styleUrls:['domain.component.css']
 })
-export class DomainComponent {
+export class DomainComponent implements OnInit{
   currentPage:number = 2;
   totalItems:number = 200;
+  domainList:Array<Domain> = [
+    {id:1,sysname:'XOPS',domainname:'www.xops.xpaas.com'},
+    {id:2,sysname:'Ticket',domainname:'wwww.hiticket.xpaas.com'}
+  ];
+  
+  ngOnInit(){
+    
+  }
+
+  search(pageIndex){
+
+  }
+
+}
+
+
+
+export class Domain{
+   id:number;
+   sysname:string;
+   domainname:string;
 }
